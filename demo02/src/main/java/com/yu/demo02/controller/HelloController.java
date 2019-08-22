3886953044079c9402cfb2084331ffa61f6e8b90
VickyElva <709334386@qq.com>
2019-08-22 17:02:04 +0800

    Initial commit

---
+++ demo02/src/main/java/com/yu/demo02/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.yu.demo02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/say")
+    public String say() {
+        return "hello world";
+    }
+}
---
+++ src/com/yu/Main.java
@@ -0,0 +1,11 @@
+package com.yu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        if(true){
+            throw new RuntimeException();
+        }
+    }
+}
